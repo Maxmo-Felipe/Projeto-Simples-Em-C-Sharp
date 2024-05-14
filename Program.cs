b1717678bec834911b56357f499859611274cb25
Maxmo-Felipe <maxmofelipeads@gmail.com>
2024-05-14 17:35:10 -0300

    Aqui começa esse novo projeto para teste

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("#######- Jogo Matématica - ####### \n");
+
+Console.WriteLine("Quanto é 2 + 3: ");
+int pergunta_1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Quanto é 10 / 2: ");
+int pergunta_2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Quanto é 20 * 2: ");
+int pergunta_3 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Quanto é 10 - 5: ");
+int pergunta_4 = Convert.ToInt32(Console.ReadLine());
+
+if((pergunta_1 == 5) && (pergunta_2 == 5) && (pergunta_3 == 40) && (pergunta_4 == 5)){
+    Console.WriteLine("####PARABÉNS####Você acertou todas e fez 4 Pontos!");
+}else if((pergunta_1 != 5) && (pergunta_2 == 5) && (pergunta_3 == 40) && (pergunta_4 == 5)){
+    Console.WriteLine("Você acertou 3 perguntas e fez 3 Pontos");
+}
